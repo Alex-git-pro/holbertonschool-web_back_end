699d2e0a5cbc3f2fdef7e4eefea493741736de0e
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-03-26 17:22:04 +0700

    Add files

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,18 +1,35 @@
 module.exports = {
     env: {
-      node: true,
-      es2021: true,
+      browser: false,
+      es6: true,
+      jest: true,
     },
     extends: [
-      'eslint:recommended',
-      'plugin:node/recommended',
+      'airbnb-base',
+      'plugin:jest/all',
     ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
     parserOptions: {
-      ecmaVersion: 12,
+      ecmaVersion: 2018,
       sourceType: 'module',
     },
+    plugins: ['jest'],
     rules: {
       'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
     },
+    overrides:[
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      }
+    ]
   };
-  
