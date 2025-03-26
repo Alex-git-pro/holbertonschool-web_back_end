240b33adb7184980765f412c28ab1d06c93a2b00
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-03-26 17:20:23 +0700

    Add files

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,10 +3,10 @@ export default function taskBlock(trueOrFalse) {
     let task2 = true;
   
     if (trueOrFalse) {
-      let task = true;  // This 'task' variable is scoped inside the if block
-      let task2 = false;  // This 'task2' variable is also scoped inside the if block
+      task = true;
+      task2 = false;
     }
   
-    return [task, task2];  // Return the original 'task' and 'task2' values
+    return [task, task2];
   }
   
