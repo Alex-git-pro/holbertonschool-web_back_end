4af794dc3973bcfb640167669f566cf807cd56d9
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-03-26 17:22:52 +0700

    Add files

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,19 +1,14 @@
-// taskFirst function: Use const since 'task' is not reassigned.
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
   }
   
-  // Helper function that returns a string
   export function getLast() {
     return ' is okay';
   }
   
-  // taskNext function: Use let since 'combination' is reassigned.
   export function taskNext() {
     let combination = 'But sometimes let';
-    combination += getLast();  // 'combination' is reassigned here
-  
+    combination += getLast();
     return combination;
   }
-  
