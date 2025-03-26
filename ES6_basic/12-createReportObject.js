374eb677f20be4b76d30bdb0f509cbe78540c05f
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-03-26 17:36:44 +0700

    Add files

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,10 @@
 export default function createReportObject(employeesList) {
     return {
-      allEmployees: { ...employeesList }, // Spread the employeesList to avoid direct mutation
-      getNumberOfDepartments: function() {
-        return Object.keys(employeesList).length; // Count the number of keys in the employeesList (departments)
-      }
+      allEmployees: {
+        ...employeesList,
+      },
+      getNumberOfDepartments() {
+        return Object.keys(this.allEmployees).length;
+      },
     };
   }
-  
