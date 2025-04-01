55d69fb820a83c2820c0fbd8d4f7a15bc2a2c956
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-04-02 03:00:14 +0700

    Push

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,3 +1,13 @@
-export default function getStudentsByLocation(students, city) {
-  return students.filter(student => student.location === city);
+function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
+
+export default handleResponseFromAPI;
