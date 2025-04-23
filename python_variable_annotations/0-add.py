3e53307dfff6ada84f7cd0c0ceef5ed8b09b9b66
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-04-23 17:46:53 +0700

    Add files

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for add function with type annotations."""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two float numbers.
+
+    Args:
+        a (float): First number.
+        b (float): Second number.
+
+    Returns:
+        float: The result of a + b.
+    """
+    return a + b
