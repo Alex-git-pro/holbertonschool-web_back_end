5698ac355ad63075f653118ce9d82642e5558d9e
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-05-20 10:18:44 +0700

    Fix: use stdout.write for initial prompt to match checker

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdin.setEncoding('utf8');
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.trim();
