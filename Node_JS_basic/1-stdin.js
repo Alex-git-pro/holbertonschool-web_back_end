28ec556f4dc162b4c4975a7c06a5b558043a8da2
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-05-20 10:14:56 +0700

    Task 1: Read user input from stdin and print name with closing message

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   const name = data.trim();
   console.log(`Your name is: ${name}`);
-  process.exit(0);
+  process.exit();
 });
 
 process.on('exit', () => {
