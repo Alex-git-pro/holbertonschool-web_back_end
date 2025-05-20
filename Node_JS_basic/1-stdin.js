642f6219c7224328e7f654ca26d07988153a013d
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-05-20 16:43:35 +0700

    Try with prettier and run format prettier

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding("utf8");
+process.stdin.setEncoding('utf8');
 
-process.stdin.once("data", (input) => {
+process.stdin.once('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
   process.stdin.end();
 });
 
-process.stdin.on("end", () => {
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
