64e170495d60901edc2beb077b37006e8fa1492d
Alex-git-pro <alexandre.mousse.git@gmail.com>
2025-05-21 16:28:29 +0700

    fix Task 6

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello Holberton School!");
-});
+// Create a small HTTP server using Express
 
+const express = require('express');
+
+const app = express();
 app.listen(1245);
+
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+
 module.exports = app;
